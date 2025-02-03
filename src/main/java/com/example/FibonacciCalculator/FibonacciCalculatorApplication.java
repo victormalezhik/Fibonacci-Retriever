461cd48d88960ce1fcee571982b7e7a2d20905f7
Legion <victormalezhik@gmail.com>
2025-02-03 14:20:25 +0100

    Initial commit

---
+++ src/main/java/com/example/FibonacciCalculator/FibonacciCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.FibonacciCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibonacciCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibonacciCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FibonacciCalculator/FibonacciCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FibonacciCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonacciCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
